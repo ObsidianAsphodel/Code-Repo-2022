a2839d317a02b7adb4b8567ff75bceaece9da35a
Terence Dixon <terencep.dixon@gmail.com>
2022-07-10 16:08:01 -0700

    init commit

---
+++ Hello-World.py
@@ -0,0 +1,9 @@
+string = "Hello World"
+input(" ")
+number1 = 20
+number2 = 304992
+print(string)
+print(input)
+print(number1 + number2)
+number3 = number1 + number2
+print(input + " points: " + number3)
