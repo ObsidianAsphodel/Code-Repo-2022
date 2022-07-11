65405d731a37c891ef10a28d94893b912863df29
Terence Dixon <terencep.dixon@gmail.com>
2022-07-10 17:50:20 -0700

    just trying out python syntax

--- Hello-World.py
+++ Hello-World.py
@@ -1,9 +1,39 @@
 string = "Hello World"
-input(" ")
+name = input(" ")
 number1 = 20
 number2 = 304992
 print(string)
 print(input)
 print(number1 + number2)
 number3 = number1 + number2
-print(input + " points: " + number3)
+number4 = input()
+print(number4)
+print(name)
+print(name + "'s points: " + number4)
+print(string * number1)
+#if/then statement
+if number2 > number1:
+    {
+        print("Nice")
+    }
+elif number2 < number1:
+    {
+        
+    } 
+else:
+    {
+        print("Damn")   
+    }
+#python function    
+def newFunction():
+    print("Hello World")
+newFunction()
+
+#Class
+class NewClass:
+    classNumber = 409
+    def newFunc():
+        {
+            
+        }
+print(NewClass().classNumber)
